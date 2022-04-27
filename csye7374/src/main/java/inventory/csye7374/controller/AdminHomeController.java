74371c3cfdc4096749fda2fab4a1b70bbd9b8589
Surbhi Parihar <surbhiparihar@Surbhis-MacBook-Air.local>
2022-04-27 15:41:03 -0400

    Added the AdminHomeController.java

--- csye7374/src/main/java/inventory/csye7374/controller/AdminHomeController.java
+++ csye7374/src/main/java/inventory/csye7374/controller/AdminHomeController.java
@@ -1,5 +1,27 @@
 package inventory.csye7374.controller;
 
-public class AdminHomeController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AdminHomeController {
+	
+	
+	@RequestMapping(value="/orderList")
+	public ModelAndView orderList(HttpServletResponse response) throws IOException{
+		return new ModelAndView("orderList");
+	}
+	
+	@RequestMapping(value="/updateOrder")
+	public ModelAndView updateOrder(HttpServletResponse response) throws IOException{
+		return new ModelAndView("updateOrder");
+	}
+	
+	@RequestMapping(value="/checkInventory")
+	public ModelAndView checkInventory(HttpServletResponse response) throws IOException{
+		return new ModelAndView("checkInventory");
+	}
 }
